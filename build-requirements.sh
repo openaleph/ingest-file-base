9867529a09eaf210c043ffdc043e61b3f240bd82
Simon Wörpel <simon.woerpel@pm.me>
2024-03-04 00:09:59 +0100

    Simplify spacy build

--- build-requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "" > spacy-requirements.txt
-
-while read m; do
-    echo "looking up $m ..."
-    echo "`spacy info $m --url`" >> spacy-requirements.txt
-done < "./spacy-models.txt"
